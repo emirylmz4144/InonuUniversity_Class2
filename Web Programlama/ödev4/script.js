3bffba7f65edaea910536fcd08d8eb70b085859a
Emir Yılmaz <emiryilmaz4144@gmail.com>
2023-07-03 14:31:07 +0300

    homeworks of "Web Programlama" class added

---
+++ Web Programlama/ödev4/script.js
@@ -0,0 +1,17 @@
+function showDetails(productId) {
+    if (productId === 1) {
+      window.open('baklava.html', '_blank');
+    } else if (productId === 2) {
+      window.open('browni.html', '_blank');
+    } else if (productId === 3) {
+      window.open('ıslakKeK.html', '_blank');
+    } else if (productId === 4) {
+      window.open('lokum.html', '_blank');
+    } else if (productId === 5) {
+      window.open('sogukBaklava.html', '_blank');
+    } else if (productId === 6) {
+      window.open('tulumba.html', '_blank');
+    } else {
+      alert('Ürün bulunamadı.');
+    }
+  }
